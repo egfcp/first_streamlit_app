0f9ce512da74e1f99a83dd7d3edc91f93122fec0
egfcp <125647139+egfcp@users.noreply.github.com>
2023-02-17 10:20:59 +0100

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
+import pandas
 
-streamlit.title('Para bailar la bamba')
-streamlit.header('\n Para bailar la bamba se necesita:')
-streamlit.text('\n -Una poca de gracia \n -Una poca de gracia \n -Y arriba y arriba')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
