9c373b06437d9ac41ce37db90ccafd5f94fc2eae
egfcp <125647139+egfcp@users.noreply.github.com>
2023-02-17 10:16:17 +0100

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 
-streamlit.title('Para bailar la bamba \n Para bailar la bamba \n Para bailar la bamba se necesita una poca de gracia')
+streamlit.title('Para bailar la bamba')
+streamlit.text(' \n Para bailar la bamba se necesita:\n -Una poca de gracia \n -Una poca de gracia \n -Y arriba y arriba')
